9ebcd0a4cfb14e1ce2206a115e7f6457b1235d3c
DavHau <hsngrmpf+github@gmail.com>
2024-06-28 16:12:29 +0700

    update /etc/hosts

--- modules/nixos/etc-hosts.nix
+++ modules/nixos/etc-hosts.nix
@@ -2,9 +2,10 @@
   networking.extraHosts =
     # home
     ''
-      192.168.194.1 dave dreambook
-      192.168.194.3 manu-macbook
-      192.168.194.2 nas
-      10.241.225.42 manu-nas
+      192.168.194.1   dave
+      192.168.194.3   manu-macbook
+      192.168.194.2   nas
+      10.241.225.42   manu-nas
+      10.147.17.2     tossa
     '';
 }
