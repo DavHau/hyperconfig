30bf34405375ca56a5c6dcf6d4fa3e7e3a80b3b1
DavHau <hsngrmpf+github@gmail.com>
2024-08-01 19:57:17 +0700

    dyndns: cleanup

--- modules/nixos/host-nas/dyndns.nix
+++ modules/nixos/host-nas/dyndns.nix
@@ -9,20 +9,6 @@
       cat $prompts/secretkey > $out/secretkey
     '';
   };
-  # clan.core.facts.services.porkbun-apikey = {
-  #   secret.apikey = {};
-  #   generator.prompt = "Enter your porkbun apikey";
-  #   generator.script = ''
-  #     echo $prompt_value > $secrets/apikey
-  #   '';
-  # };
-  # clan.core.facts.services.porkbun-secretkey = {
-  #   secret.secretkey = {};
-  #   generator.prompt = "Enter your porkbun secretkey";
-  #   generator.script = ''
-  #     echo $prompt_value > $secrets/secretkey
-  #   '';
-  # };
   systemd.timers.porkbun-dyndns = {
     description = "Update porkbun dynamic dns";
     wantedBy = [ "timers.target" ];
@@ -58,24 +44,5 @@
         }
       "
     '';
-    # script = ''
-    #   set -e
-    #   ipv4=$(curl -4 --silent --fail ifconfig.co)
-    #   ipv6=$(curl -6 --silent --fail ifconfig.co)
-    #   curl https://porkbun.com/api/json/v3/dns/editByNameType/bruch-bu.de/A/casa -d "
-    #     {
-    #       \"apikey\": \"$(cat ${config.clan.core.facts.services.porkbun-apikey.secret.apikey.path})\",
-    #       \"secretapikey\": \"$(cat ${config.clan.core.facts.services.porkbun-secretkey.secret.secretkey.path})\",
-    #       \"content\": \"$ipv4\"
-    #     }
-    #   "
-    #   curl https://porkbun.com/api/json/v3/dns/editByNameType/bruch-bu.de/AAAA/casa -d "
-    #     {
-    #       \"apikey\": \"$(cat ${config.clan.core.facts.services.porkbun-apikey.secret.apikey.path})\",
-    #       \"secretapikey\": \"$(cat ${config.clan.core.facts.services.porkbun-secretkey.secret.secretkey.path})\",
-    #       \"content\": \"$ipv6\"
-    #     }
-    #   "
-    # '';
   };
 }
