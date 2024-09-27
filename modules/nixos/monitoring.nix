4f2425220313df90bd944fcd23c454ce2041e379
DavHau <hsngrmpf+github@gmail.com>
2024-09-27 11:45:26 +0200

    monitoring: enable zerotier by default

--- modules/nixos/monitoring.nix
+++ modules/nixos/monitoring.nix
@@ -1,8 +1,11 @@
+# Every device needs to be added to https://github.com/Mic92/dotfiles/blob/main/machines/eva/modules/telegraf/dave.nix
+
 {inputs, ...}: {
   imports = [
     inputs.srvos.nixosModules.mixins-telegraf
   ];
 
+  services.zerotierone.enable = true;
   services.zerotierone.joinNetworks = [
     "b15644912e61dbe0"  # monitoring
   ];
