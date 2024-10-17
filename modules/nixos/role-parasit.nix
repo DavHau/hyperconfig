60eb48ece293ed4002da5c6ca60f20a3ed988366
DavHau <hsngrmpf+github@gmail.com>
2024-10-17 18:55:25 +0700

    update role-parasit

--- modules/nixos/role-parasit.nix
+++ modules/nixos/role-parasit.nix
@@ -1,11 +1,18 @@
 {
   config,
+  pkgs,
   ...
-}: {
+}:
+let
+  secretFile = pkgs.runCommand "wifi-parasit" {} ''
+    echo PW:"$(cat ${config.sops.secrets.wifi-parasit.path})" > $out
+  '';
+in
+{
   # wifi
   networking.wireless.enable = true;
-  networking.wireless.networks.Parasit_5G.psk = "@PW@";
+  networking.wireless.secretsFile = secretFile;
+  networking.wireless.networks.Parasit_5G.auth = "password=ext:PW";
   networking.wireless.networks.Parasit_5G.priority = 10;
-  networking.wireless.networks.Parasit.psk = "@PW@";
-  networking.wireless.environmentFile = config.sops.secrets.wifi-parasit.path;
+  networking.wireless.networks.Parasit.auth = "password=ext:PW";
 }
