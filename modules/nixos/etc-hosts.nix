f1e47ee84eb441581c93f95eed5d15036e6a8f49
DavHau <hsngrmpf+github@gmail.com>
2024-10-29 16:14:35 +0700

    update hosts

--- modules/nixos/etc-hosts.nix
+++ modules/nixos/etc-hosts.nix
@@ -6,7 +6,8 @@
       192.168.194.3   manu-macbook
       192.168.194.2   nas
       10.241.225.42   manu-nas
-      10.147.17.2     tossa
+      10.147.17.2     router-tossa tossa
       192.168.192.1   introducer
+      192.168.194.20  router-waldkirch waldkirch
     '';
 }
