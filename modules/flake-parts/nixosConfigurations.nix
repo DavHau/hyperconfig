357f99b04878278adccff838d017491de21b186d
DavHau <hsngrmpf+github@gmail.com>
2024-12-09 16:21:34 +0700

    updates

--- modules/flake-parts/nixosConfigurations.nix
+++ modules/flake-parts/nixosConfigurations.nix
@@ -5,26 +5,16 @@
   specialArgs = {
     inherit inputs pkgs-unstable self;
   };
-  defaultModules = [ ];
-  # collect all nixos modules which define hosts, prefixed with `host-`.
-  hostModules' =
-    l.filterAttrs (name: module: l.hasPrefix "host-" name) self.modules.nixos;
-
-  # remove the `host-` prefix for the configuration names;
-  hostModules = l.flip l.mapAttrs' hostModules'
-    (name: module: {name = l.removePrefix "host-" name; value = module;});
 
 in {
   flake = inputs.clan-core.lib.buildClan {
     directory = self;
     inherit specialArgs;
-    meta.name = "DavClan";
-    # machines = l.flip l.mapAttrs hostModules (name: module: {
-    #   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-    #   imports =
-    #     defaultModules
-    #     ++ [module]
-    #     ++[{networking.hostName = name;}];
-    # });
+    # meta.name = "DavClan";
+    inventory.services.zerotier.zt-home = {
+      roles.peer.tags = [ "all" ];
+      roles.controller.machines = [ "nas" ];
+      # roles.moon.machines = [ "nas" ];
+    };
   };
 }
