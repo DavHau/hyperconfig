53c82a88c9556bbbee457e694edfb26dc077dafd
DavHau <hsngrmpf+github@gmail.com>
2024-12-12 11:42:07 +0700

    cm-pi: init

--- machines/cm-pi/configuration.nix
+++ machines/cm-pi/configuration.nix
@@ -2,20 +2,18 @@
   imports = [
     inputs.nixos-generators.nixosModules.all-formats
     ../../modules/nixos/common.nix
-    ../../modules/nixos/dyndns-porkbun.nix
     ../../modules/nixos/monitoring.nix
+    ../../modules/nixos/role-flixbus.nix
     ./hardware-configuration.nix
-    ./reverse-proxy.nix
   ];
   nixpkgs.hostPlatform = "aarch64-linux";
-  networking.wireless.enable = true;
   networking.useDHCP = true;
   formatConfigs.sd-aarch64 = {
     disabledModules = [
       ./hardware-configuration.nix
     ];
   };
-  clan.core.networking.targetHost = "root@192.168.10.21";
+  # clan.core.networking.targetHost = "root@192.168.10.21";
   clan.core.networking.buildHost = "grmpf@localhost";
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
