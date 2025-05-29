608a2ddb5121167703e63b1761ce29c19eb11e35
DavHau <hsngrmpf+github@gmail.com>
2025-05-29 21:45:32 +0700

    bam: benchmark many filesystems + settings

--- machines/bam/configuration.nix
+++ machines/bam/configuration.nix
@@ -1,7 +1,7 @@
 {
   imports = [
     ../../modules/nixos/common.nix
-    ./disk.nix
+    ./disko-xfs.nix
   ];
   nixpkgs.hostPlatform = "x86_64-linux";
 }
---
+++ machines/bam/disko-zfs-defaults.nix
@@ -0,0 +1,71 @@
+# TODO: !!!!!!!!!
+# PLEASE ADD SOME SWAP TO THE NEXT SERVER!
+
+{ config, pkgs, ... }:
+{
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+
+  disko.devices = {
+    disk = {
+      x = rec {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-WD_BLACK_SN7100_2TB_251653801553";
+        content = {
+          type = "gpt";
+          partitions = {
+            "boot" = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+              priority = 1;
+            };
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          };
+        };
+      };
+    };
+    zpool = {
+      zroot = {
+        type = "zpool";
+        # rootFsOptions = {
+        #   compression = "lz4";
+        #   "com.sun:auto-snapshot" = "true";
+        # };
+        datasets = {
+          "root" = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "none";
+              encryption = "aes-256-gcm";
+              keyformat = "hex";
+              keylocation = "file://${zfs-key}";
+            };
+          };
+          "root/nixos" = {
+            type = "zfs_fs";
+            options.mountpoint = "/";
+            mountpoint = "/";
+          };
+        };
+      };
+    };
+  };
+}
