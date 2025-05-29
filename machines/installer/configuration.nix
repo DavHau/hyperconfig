9643419cc81befa23d3663d463ccdcf7d5b51ace
DavHau <hsngrmpf+github@gmail.com>
2025-05-29 21:45:46 +0700

    add installer machine

---
+++ machines/installer/configuration.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  clan-core,
+  inputs,
+  lib,
+  ...
+}:
+{
+  imports = [
+    clan-core.clanModules.trusted-nix-caches
+  ];
+
+  networking.hostName = "nixos-installer";
+
+  clan.core.deployment.requireExplicitUpdate = true;
+
+  nixpkgs.pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+  system.stateVersion = config.system.nixos.release;
+
+  image.modules.kexecTarball = {config, ...}: {
+    imports = [
+      inputs.nixos-images.nixosModules.kexec-installer
+    ];
+    system.build.image = lib.mkForce config.system.build.kexecInstallerTarball;
+
+    # needed to prevent conflict in module eval
+    systemd.network.networks."99-ethernet-default-dhcp".networkConfig.MulticastDNS = true;
+    systemd.network.networks."99-wireless-client-dhcp".networkConfig.MulticastDNS = true;
+  };
+}
