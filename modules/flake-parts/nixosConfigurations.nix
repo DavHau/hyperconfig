8a7f7bff77f762c0eed6822a9c7e0055d3533f41
DavHau <hsngrmpf+github@gmail.com>
2025-08-03 20:17:17 +0700

    nixosConfigurations: update to latest clan api

--- modules/flake-parts/nixosConfigurations.nix
+++ modules/flake-parts/nixosConfigurations.nix
@@ -13,87 +13,98 @@
   );
 
 in {
-  flake.clan = (inputs.clan-core.lib.clan {
-    inherit self;
+  flake =
+    let
+      clan = (inputs.clan-core.lib.clan {
+        inherit self;
 
-    pkgsForSystem = system: import inputs.nixpkgs {
-      inherit system;
-      # config = {
-      #   replaceStdenv = ({ pkgs }: pkgs.withCFlags [ "-funroll-loops" "-O3" "-march=x86-64-v3" ] pkgs.stdenv);
-      # };
-      # config.contentAddressedByDefault = true;
-    };
+        pkgsForSystem = system: import inputs.nixpkgs {
+          inherit system;
+          # config = {
+          #   replaceStdenv = ({ pkgs }: pkgs.withCFlags [ "-funroll-loops" "-O3" "-march=x86-64-v3" ] pkgs.stdenv);
+          # };
+          # config.contentAddressedByDefault = true;
+        };
 
-    specialArgs = {
-      inherit inputs pkgsCross self;
-    };
+        specialArgs = {
+          inherit inputs pkgsCross self;
+        };
 
-    meta.name = "DavClan";
+        meta.name = "DavClan";
 
-    modules = {
-      nix-cache = ../../modules/clan/nix-cache;
-      easytier = ../../modules/clan/easytier;
-    };
+        modules = {
+          nix-cache = ../../modules/clan/nix-cache;
+          easytier = ../../modules/clan/easytier;
+        };
 
-    # add machines to their hosts
-    inventory = {
-      machines = {
-        bam.tags = [ "wifi-home"];
-        installer.tags = [ "wifi-home" ];
-        cm-pi.tags = [ "wifi-home" ];
+        # add machines to their hosts
+        inventory = {
+          machines = {
+            bam.tags = [ "wifi-home"];
+            installer.tags = [ "wifi-home" ];
+            cm-pi.tags = [ "wifi-home" ];
+          };
+          services = {
+            importer.base = {
+              roles.default.tags = ["all"];
+              roles.default.extraModules = [
+                inputs.clan-core.clanModules.static-hosts
+                ../../modules/nixos/common.nix
+              ];
+            };
+            zerotier.zt-home = {
+              roles.peer.tags = [ "all" ];
+              roles.controller.machines = [ "nas" ];
+              # roles.moon.machines = [ "nas" ];
+            };
+          };
+
+          # NEW API
+          instances = {
+            wifi-home = {
+              module.name = "wifi";
+              module.input = "clan-core";
+              roles.default.settings.networks.home = {};
+              roles.default.tags.wifi-home = {};
+            };
+            dave-cache = {
+              module.name = "nix-cache";
+              module.input = "self";
+              roles.server.machines.bam = {};
+              roles.server.settings.priority = 41;
+              roles.client.tags.all = {};
+            };
+            dave-backup = {
+              module.name = "borgbackup";
+              module.input = "clan-core";
+              roles.server.machines.nas = {};
+              roles.client.machines.amy = {};
+              roles.client.machines.amy = {};
+              roles.client.settings.exclude = import ../backup-exclude.nix;
+              roles.server.settings.directory = "/pool11/enc/clan-backup";
+            };
+            dave = {
+              module.name = "easytier";
+              module.input = "self";
+              # roles.peer.settings.domain = "dave";
+              roles.peer.settings.foreignHostNames = [
+                "ashburn1"
+                "buremberg1"
+              ];
+              roles.peer.tags.all = {};
+            };
+            sshd = {
+              module.name = "sshd";
+              module.input = "clan-core";
+              roles.server.tags.all = {};
+              roles.client.tags.all = {};
+            };
+          };
+        };
+      });
+    in
+      {
+        inherit (clan.config) clanInternals nixosConfigurations;
+        clan = clan.config;
       };
-      services = {
-        importer.base = {
-          roles.default.tags = ["all"];
-          roles.default.extraModules = [
-            inputs.clan-core.clanModules.static-hosts
-            ../../modules/nixos/common.nix
-          ];
-        };
-        zerotier.zt-home = {
-          roles.peer.tags = [ "all" ];
-          roles.controller.machines = [ "nas" ];
-          # roles.moon.machines = [ "nas" ];
-        };
-      };
-
-      # NEW API
-      instances = {
-        wifi-home = {
-          module.name = "wifi";
-          module.input = "clan-core";
-          roles.default.settings.networks.home = {};
-          roles.default.tags.wifi-home = {};
-        };
-        dave-cache = {
-          module.name = "nix-cache";
-          module.input = "self";
-          roles.server.machines.bam = {};
-          roles.server.settings.priority = 41;
-          roles.client.tags.all = {};
-        };
-        dave-backup = {
-          module.name = "borgbackup";
-          module.input = "clan-core";
-          roles.server.machines.nas = {};
-          roles.client.machines.amy = {};
-          roles.client.machines.amy = {};
-          roles.client.settings.exclude = import ../backup-exclude.nix;
-          roles.server.settings.directory = "/pool11/enc/clan-backup";
-        };
-        dave = {
-          module.name = "easytier";
-          module.input = "self";
-          roles.peer.settings.domain = "dave";
-          roles.peer.tags.all = {};
-        };
-        sshd = {
-          module.name = "sshd";
-          module.input = "clan-core";
-          roles.server.tags.all = {};
-          roles.client.tags.all = {};
-        };
-      };
-    };
-  }).config;
 }
