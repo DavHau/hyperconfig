e3598a1e1a932cf7bc236f14828c278e6b0232d2
DavHau <hsngrmpf+github@gmail.com>
2025-09-30 14:33:44 +0700

    update dns

--- modules/nixos/dns.nix
+++ modules/nixos/dns.nix
@@ -1,17 +1,22 @@
 {
-  # networking.nameservers = [
-  #   "1.1.1.1"
-  #   "1.0.0.1"
-  # ];
+  networking.nameservers = [
+    "1.1.1.1"
+    "1.0.0.1"
+  ];
 
   services.resolved = {
     enable = true;
-    dnssec = "allow-downgrade";
+    # dnssec = "allow-downgrade";
+    dnssec = "true";
     domains = [ "~." ];
     fallbackDns = [
       "1.1.1.1"
       "1.0.0.1"
     ];
-    dnsovertls = "opportunistic";
+    # dnsovertls = "opportunistic";
+    dnsovertls = "true";
   };
+
+  programs.captive-browser.enable = true;
+  programs.captive-browser.interface = "wlp1s0";
 }
