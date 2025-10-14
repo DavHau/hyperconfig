6728eafdc66fa339d92b491c40d1777f60950007
DavHau <hsngrmpf+github@gmail.com>
2025-10-14 11:08:10 +0700

    resolved: back to defaults

--- modules/nixos/dns.nix
+++ modules/nixos/dns.nix
@@ -1,20 +1,20 @@
 {
-  networking.nameservers = [
-    "1.1.1.1"
-    "1.0.0.1"
-  ];
+  # networking.nameservers = [
+  #   "1.1.1.1"
+  #   "1.0.0.1"
+  # ];
 
   services.resolved = {
     enable = true;
     # dnssec = "allow-downgrade";
-    dnssec = "true";
-    domains = [ "~." ];
-    fallbackDns = [
-      "1.1.1.1"
-      "1.0.0.1"
-    ];
+    # dnssec = "true";
+    # domains = [ "~." ];
+    # fallbackDns = [
+    #   "1.1.1.1"
+    #   "1.0.0.1"
+    # ];
     # dnsovertls = "opportunistic";
-    dnsovertls = "true";
+    # dnsovertls = "true";
   };
 
   programs.captive-browser.enable = true;
