78f1d390a52f079f884c84adbdc85418dd6d437e
DavHau <hsngrmpf+github@gmail.com>
2025-10-14 11:07:09 +0700

    remove avahi from printing.nix

--- machines/amy/printing.nix
+++ machines/amy/printing.nix
@@ -1,8 +1,4 @@
 {
   services.printing.enable = true;
-  services.avahi.enable = true;
-  services.avahi.nssmdns4 = true;
-  # for a WiFi printer
-  services.avahi.openFirewall = true;
   services.printing.logLevel = "debug";
 }
