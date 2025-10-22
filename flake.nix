e1b3ff86692e3e1c5ce25214625db8560584741e
DavHau <hsngrmpf+github@gmail.com>
2025-10-22 11:29:27 +0700

    riscv: switch to dave nixpkgs branch for orjson fixes

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     nixpkgs.url = "git+https://github.com/nixos/nixpkgs?&ref=nixos-unstable&shallow=1";
     # nixpkgs.url = "git+https://github.com/DavHau/nixpkgs?&ref=dave&shallow=1";
     # nixpkgs-riscv.url = "git+https://github.com/davhau/nixpkgs?&ref=riscv&shallow=1";
-    nixpkgs-riscv.follows = "nixpkgs";
+    nixpkgs-riscv.url = "git+https://github.com/DavHau/nixpkgs?&ref=dave&shallow=1";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
