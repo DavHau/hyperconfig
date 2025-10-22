2670cb20f817396ed752e5401e1caf4aceeb22d7
DavHau <hsngrmpf+github@gmail.com>
2025-10-22 11:26:31 +0700

    caches: remove cache-low-prio.clan.lol

--- modules/nixos/nix-caches.nix
+++ modules/nixos/nix-caches.nix
@@ -1,9 +1,9 @@
 {
   nix.settings.trusted-substituters = [
-    "https://cache-low-prio.clan.lol"
+    "https://cache.clan.lol"
   ];
   nix.settings.substituters = [
-    "https://cache-low-prio.clan.lol"
+    "https://cache.clan.lol"
   ];
   nix.settings.trusted-public-keys = [
     "cache.clan.lol-1:3KztgSAB5R1M+Dz7vzkBGzXdodizbgLXGXKXlcQLA28="
