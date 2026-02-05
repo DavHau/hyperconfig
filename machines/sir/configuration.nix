8a5e0efac37764810517f587d961df54f3c8dbd1
DavHau <hsngrmpf+github@gmail.com>
2026-02-05 15:26:02 +0700

    sir: add config

--- machines/sir/configuration.nix
+++ machines/sir/configuration.nix
@@ -1,7 +1,25 @@
-{
+{inputs, ...}: {
   imports = [
+    inputs.srvos.nixosModules.mixins-cloud-init
 
+    ../../modules/nixos/common.nix
+    ../../modules/nixos/common-tools.nix
+    ../../modules/nixos/nix-caches.nix
   ];
 
-  # New machine!
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/f222513b-ded1-49fa-b591-20ce86a2fe7f";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/12CE-A600";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  networking.useDHCP = true;
 }
