e2e9983f3b17dd073256a486c01b6ce15f0499d5
DavHau <hsngrmpf+github@gmail.com>
2026-03-12 13:03:46 +0900

    ollama: override to latest version

--- overlays/ollama.nix
+++ overlays/ollama.nix
@@ -1,11 +1,11 @@
 final: prev: {
   ollama = prev.ollama.overrideAttrs (old: rec {
-    version = "0.17.4";
+    version = "0.17.5";
     src = final.fetchFromGitHub {
       owner = "ollama";
       repo = "ollama";
       tag = "v${version}";
-      hash = "sha256-9yJ8Jbgrgiz/Pr6Se398DLkk1U2Lf5DDUi+tpEIjAaI=";
+      hash = "sha256-MPcLs9O7GZoPLnpGq3LQU13j6Nhhb4InoeXLts6yncU=";
     };
     vendorHash = "sha256-Lc1Ktdqtv2VhJQssk8K1UOimeEjVNvDWePE9WkamCos=";
   });
