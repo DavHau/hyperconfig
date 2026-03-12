b1e1eba5b93bd046a55fc945a6b479571fb1975e
DavHau <hsngrmpf+github@gmail.com>
2026-03-12 13:00:24 +0900

    remove direnv from home-manager
    already configured in nixos

--- machines/amy/home-manager.nix
+++ machines/amy/home-manager.nix
@@ -28,9 +28,6 @@
       font.normal.family = "FiraCode Nerd Font";
     };
     programs.alacritty.package = pkgs.alacritty;
-
-    programs.direnv.enable = true;
-    programs.direnv.nix-direnv.enable = true;
     # optional for nix flakes support in home-manager 21.11, not required in home-manager unstable or 22.05
 
     programs.zoxide.enable = true;
