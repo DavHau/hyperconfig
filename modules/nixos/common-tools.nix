4f2989d92089be72415b1587f4ea956f6233625c
DavHau <hsngrmpf+github@gmail.com>
2026-03-25 20:27:28 +0800

    direnv.sandbox: update version and binds

--- modules/nixos/common-tools.nix
+++ modules/nixos/common-tools.nix
@@ -21,18 +21,21 @@
   programs.direnv.enable = true;
   programs.direnv.sandbox = {
     enable = true;
-    allowParent = "read";
+    allowParent = "write";
     persist = [
       "$HOME/.claude"
     ];
     bind = {
-      "$HOME/.pi" = {};
+      "$HOME/.pi/agent/auth.json" = {};
+      "$HOME/.pi/agent/settings.json" = {};
       "$HOME/.local/share/zoxide" = {};
       "$HOME/.claude/.credentials.json" = {};
       # VSCode state/config (caches, workspaces, settings, etc.)
       "$HOME/.config/Code" = {};
     };
     bindReadOnly = {
+      "$HOME/.pi/agent/skills" = {};
+      "$HOME/.pi/agent/models.json" = {};
       "$HOME/.ssh/id_ed25519_github1".to = "$HOME/.ssh/id_ed25519";
       "$HOME/.ssh/id_ed25519_github1.pub".to = "$HOME/.ssh/id_ed25519.pub";
       # VSCode extensions (nix-managed) and CLI
