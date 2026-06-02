7b7a25ee9888638aa0847ab23959aa475d2113ef
DavHau <need-more-ram@DavHau.com>
2026-06-02 08:30:22 +0000

    nix-caches: trust cache.numtide.com substituter

--- modules/nixos/nix-caches.nix
+++ modules/nixos/nix-caches.nix
@@ -1,11 +1,14 @@
 {
   nix.settings.trusted-substituters = [
     "https://cache.clan.lol"
+    "https://cache.numtide.com"
   ];
   nix.settings.substituters = [
     "https://cache.clan.lol"
+    "https://cache.numtide.com"
   ];
   nix.settings.trusted-public-keys = [
     "cache.clan.lol-1:3KztgSAB5R1M+Dz7vzkBGzXdodizbgLXGXKXlcQLA28="
+    "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
   ];
 }
