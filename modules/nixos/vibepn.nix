f406cf9ab5892b45edfc85a5c1c61c39622da6be
DavHau <need-more-ram@DavHau.com>
2026-07-18 15:32:35 +0700

    vibepn: open firewall ports for discovery + data plane

--- modules/nixos/vibepn.nix
+++ modules/nixos/vibepn.nix
@@ -7,4 +7,12 @@
     inputs.vibepn.packages.${pkgs.stdenv.hostPlatform.system}.vibepn
     inputs.vibepn.packages.${pkgs.stdenv.hostPlatform.system}.vpnd
   ];
+
+  # The upstream VM tests run firewall-free; on real machines the default
+  # NixOS firewall breaks discovery. Open:
+  #   5353/udp  mDNS local peer discovery
+  #   4001      libp2p control plane (tcp + quic) - descriptor rendezvous, hole punching
+  #   51820/udp data plane ([[network]] listen)
+  networking.firewall.allowedTCPPorts = [ 4001 ];
+  networking.firewall.allowedUDPPorts = [ 5353 4001 51820 ];
 }
