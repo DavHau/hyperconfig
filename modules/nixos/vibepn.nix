20435d0a5179b431ae69d27cddb1def4ef2f89ec
DavHau <need-more-ram@DavHau.com>
2026-07-18 20:35:14 +0700

    vibepn: close 4001/51820 firewall ports again

--- modules/nixos/vibepn.nix
+++ modules/nixos/vibepn.nix
@@ -7,13 +7,4 @@
     inputs.vibepn.packages.${pkgs.stdenv.hostPlatform.system}.vibepn
     inputs.vibepn.packages.${pkgs.stdenv.hostPlatform.system}.vpnd
   ];
-
-  # The host firewall blocked VibePN's post-discovery dials (verified: 4001 was
-  # closed on amy/bam pre-integration; 5353 was already open via the shared
-  # module stack, so mDNS discovery itself worked). Open:
-  #   4001      libp2p control plane (tcp + quic) - descriptor rendezvous, hole punching
-  #   51820/udp data plane ([[network]] listen)
-  # Remove once VibePN handles firewalled hosts (VibePN backlog/lan-firewalled-hosts.md).
-  networking.firewall.allowedTCPPorts = [ 4001 ];
-  networking.firewall.allowedUDPPorts = [ 4001 51820 ];
 }
