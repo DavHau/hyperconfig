0546a5e55d31f8958bc5ba4d40b6de8970ece0bc
DavHau <need-more-ram@DavHau.com>
2026-08-05 18:03:20 +0700

    remove some llama-swap models

--- modules/nixos/llama-swap.nix
+++ modules/nixos/llama-swap.nix
@@ -8,23 +8,8 @@ let
 in
 {
   services.llama-swap.settings.models = {
-    "qwen3.5:0.8b" = {
-      cmd = "${llama-server} -hf unsloth/Qwen3.5-0.8B-GGUF --port \${PORT}";
-    };
-    "qwen3.5:27b" = {
-      cmd = "${llama-server} -hf unsloth/Qwen3.5-27B-GGUF --port \${PORT}";
-    };
     "qwen3.5:35b" = {
       cmd = "${llama-server} -hf unsloth/Qwen3.5-35B-A3B-GGUF:Q4_K_M --port \${PORT}";
-    };
-    "qwen3.5:122b" = {
-      cmd = "${llama-server} -hf unsloth/Qwen3.5-122B-A10B-GGUF:UD-Q3_K_XL --port \${PORT}";
-    };
-    "gemma4:31b" = {
-      cmd = "${llama-server} -hf unsloth/gemma-4-31B-it-GGUF:Q4_K_M --port \${PORT}";
-    };
-    "gemma4:26b" = {
-      cmd = "${llama-server} -hf unsloth/gemma-4-26B-A4B-it-GGUF:UD-Q4_K_M --port \${PORT}";
     };
   };
 }
